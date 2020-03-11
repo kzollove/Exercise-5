a1052a9c93d547bffb636d1b5db8f1bf8ad8b5d4
kzollove <kylezollo@gmail.com>
2020-03-11 13:13:06 -0400

    Problem 4

--- data_exploration.py
+++ data_exploration.py
@@ -72,3 +72,51 @@ rovaniemi.to_csv(output_rovaniemi, sep=',', index=False, float_format="%.2f")
 
 
 ###### Problem 4 #######
+
+# In this problem the aim is to understand how different the summer temperatures has been in Helsinki Kumpula and Rovaniemi.
+# #Using the data from Problem 3 in kumpula and rovaniemi DataFrames answer to following questions:
+
+# Part 1
+# What was the median temperature in:
+# Helsinki Kumpula?
+# Rovaniemi?
+print('Median Temp K:', kumpula[["TEMP"]].median(), '\nMedian Temp R:', rovaniemi[["TEMP"]].median())
+
+
+# Part 2
+# Part 1 considers data from quite long period of time (May-Aug), hence the differences might not be so clear.
+# Let's find out what were the mean temperatures in May and June in Kumpula and Rovaniemi:
+mayK = kumpula.ix[kumpula['YR--MODAHRMN'] < 201706010000]
+junK = kumpula.ix[(kumpula['YR--MODAHRMN'] >= 201706010000) & (kumpula['YR--MODAHRMN'] < 201707010000)]
+mayR = rovaniemi.ix[rovaniemi['YR--MODAHRMN'] < 201706010000]
+junR = rovaniemi.ix[(rovaniemi['YR--MODAHRMN'] >= 201706010000) & (rovaniemi['YR--MODAHRMN'] < 201707010000)]
+
+print('Mean Temperatures in May and June of Kumpula and Rovaniemi')
+print('Kumpula, May: ',mayK['TEMP'].mean())
+print('Kumpula, Jun: ',junK['TEMP'].mean())
+print('Rovaniemi, May: ',mayR['TEMP'].mean())
+print('Rovaniemi, Jun: ',junR['TEMP'].mean())
+
+print('Min Temperatures in May and June of Kumpula and Rovaniemi')
+print('Kumpula, May: ',mayK['TEMP'].min())
+print('Kumpula, Jun: ',junK['TEMP'].min())
+print('Rovaniemi, May: ',mayR['TEMP'].min())
+print('Rovaniemi, Jun: ',junR['TEMP'].min())
+
+print('Max Temperatures in May and June of Kumpula and Rovaniemi')
+print('Kumpula, May: ',mayK['TEMP'].max())
+print('Kumpula, Jun: ',junK['TEMP'].max())
+print('Rovaniemi, May: ',mayR['TEMP'].max())
+print('Rovaniemi, Jun: ',junR['TEMP'].max())
+
+# Select from rovaniemi and kumpula DataFrames such rows from the DataFrames where YR--MODAHRMN values are from May 2017 (see hints for help) and assign them into variables rovaniemi_may and kumpula_may
+# Do similar procedure for June and assign those values into variables rovaniemi_june and kumpula_june
+# Using those new subsets print the mean, min and max temperatures for both places in May and June.
+# You can add your codes into a data_exploration.py file.
+
+# Upload the script to your GitHub repository for Exercise-5
+# Remember to comment well your code! (add docstring, and comments explaining what your code does)
+# Interpreting the results after the data analysis is one of the most important steps in a process called knowledge discovery. Hence, use the information above to discuss shortly about following questions (justify your answers with the data analysis results):
+
+# Does there seem to be large difference in temperatures between the months?
+# Is Rovaniemi much colder place than Kumpula?
